3ba8e86af2e69948bbaae083c72d08f431b37cac
Jules Ko DIOUF <jkd.gomycode@gmail.com>
2022-08-02 18:46:55 +0000

    Adding button toggle and profile container display

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  state = {
+    Person : {
+      fullName : "Jules Ko DIOUF",
+      bio : "Software engineering enthusiast !!",
+      imgSrc : "/img/profil.jpg",
+      profession : " Web | Software developer"
+    },
+    shows : false
+  };
+
+  changeState = () => (this.state.shows) ? this.setState({shows : false}) : this.setState({shows : true});
+  
+  render() {
+    
+    let view
+    if(this.state.shows){
+      view = (
+            <div className="profile_container">
+              <img src={this.state.imgSrc} alt="photo de profile" />
+              <h1>{this.state.fullName}</h1>
+              <p>{this.state.bio}</p>
+              <h5>{this.state.profession}</h5>
+            </div>
+      )
+    }
+    else
+      view = <div className="profile_container"></div>
+
+    return (
+      <div>
+        <button name='toggle_button' onClick={this.changeState}>Afficher profile</button>
+        {view}
+      </div>
+    );
+  }
 }
 
 export default App;
